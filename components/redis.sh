074efd2b145224d1a3d6345804258bd34a5ef4cb
Raghu K <raghuk.vit@gmail.com>
2022-06-17 07:28:54 +0530

    Update roboshop-shell-scirpt/

--- components/redis.sh
+++ components/redis.sh
@@ -1,1 +1,7 @@
-echo Install Redis Component
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+yum install redis-6.2.7 -y
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+systemctl enable redis
+systemctl start redis
+
