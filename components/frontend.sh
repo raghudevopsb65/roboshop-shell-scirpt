e7354acc6fc76abb21846888ef7f8130150c96ea
Raghu K <raghuk.vit@gmail.com>
2022-06-17 07:39:04 +0530

    Update roboshop-shell-scirpt/

--- components/frontend.sh
+++ components/frontend.sh
@@ -12,3 +12,5 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
