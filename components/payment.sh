f77e15b4050a3ba20c5d0c0896e9a17531ada39a
Raghu K <raghuk.vit@gmail.com>
2022-06-18 06:48:52 +0530

    Update roboshop-shell-scirpt/

--- components/payment.sh
+++ components/payment.sh
@@ -1,1 +1,20 @@
-echo Install Payment Component
+yum install python36 gcc python3-devel -y
+useradd roboshop
+cd /home/roboshop
+rm -rf payment
+curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+unzip /tmp/payment.zip
+mv payment-main payment
+cd /home/roboshop/payment
+pip3 install -r requirements.txt
+
+#1. Update the roboshop user and group id in `payment.ini` file.
+#2. Update SystemD service file
+#
+#    Update `CARTHOST` with cart server ip
+#
+#    Update `USERHOST` with user server ip
+#
+#    Update `AMQPHOST` with RabbitMQ server ip.
+#
+#3. Setup the service
