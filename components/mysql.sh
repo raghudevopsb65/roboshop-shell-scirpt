a00f0109826e26c2d584d276821374b44c5ceab4
Raghu K <raghuk.vit@gmail.com>
2022-06-18 06:26:01 +0530

    Update roboshop-shell-scirpt/

--- components/mysql.sh
+++ components/mysql.sh
@@ -6,7 +6,7 @@ systemctl start mysqld
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
-echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
+echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}"
 #```
 #
 ### **Setup Needed for Application.**
