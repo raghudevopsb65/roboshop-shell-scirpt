43e44b9100898f044447f4146cc962bf123487ac
Raghu K <raghuk.vit@gmail.com>
2022-06-20 07:26:02 +0530

    Update roboshop-shell-scirpt/

--- components/common.sh
+++ components/common.sh
@@ -9,6 +9,7 @@ CHECK_ROOT() {
 CHECK_STAT() {
 if [ $1 -ne 0 ]; then
   echo -e "\e[31mFAILED\e[0m"
+  echo -e "\n Check log file - ${LOG} for errors\n"
   exit 2
 else
   echo -e "\e[32mSUCCESS\e[0m"
