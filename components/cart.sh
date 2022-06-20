a3f14c61f1d7c1b6cc5eb8e73f471ad40a1ea177
Raghu K <raghuk.vit@gmail.com>
2022-06-20 07:14:48 +0530

    Update roboshop-shell-scirpt/

--- components/cart.sh
+++ components/cart.sh
@@ -3,6 +3,11 @@ source components/common.sh
 CHECK_ROOT
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+if [ $? -ne 0 ]; then
+  echo "Setting Up NodeJS YUM Repo is failure"
+  exit 2
+fi
+
 yum install nodejs -y
 useradd roboshop
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
