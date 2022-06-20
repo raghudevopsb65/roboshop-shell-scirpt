6f28bf6aa4c4f152d58a0145baef0e29700b2bdf
Raghu K <raghuk.vit@gmail.com>
2022-06-20 07:02:35 +0530

    Update roboshop-shell-scirpt/

--- components/cart.sh
+++ components/cart.sh
@@ -1,9 +1,13 @@
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-    echo You are Non root user
-    echo You can run this script as root user or with sudo
-    exit 1
-fi
+CHECK_ROOT() {
+  USER_ID=$(id -u)
+  if [ $USER_ID -ne 0 ]; then
+      echo You are Non root user
+      echo You can run this script as root user or with sudo
+      exit 1
+  fi
+}
+
+CHECK_ROOT
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
