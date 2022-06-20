2cdd7d46ef9ca775e61cb02881310de2738bf894
Raghu K <raghuk.vit@gmail.com>
2022-06-20 07:06:14 +0530

    Update roboshop-shell-scirpt/

--- components/common.sh
+++ components/common.sh
@@ -1,7 +1,7 @@
 CHECK_ROOT() {
   USER_ID=$(id -u)
   if [ $USER_ID -ne 0 ]; then
-      echo -e "\e[33mYou should be running this script as root user or sudo this script\e[0m"
+      echo -e "\e[31mYou should be running this script as root user or sudo this script\e[0m"
       exit 1
   fi
 }
