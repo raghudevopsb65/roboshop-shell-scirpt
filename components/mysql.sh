84a9e2c3d1152198e027367107cb8238d650a2ed
Raghu K <raghuk.vit@gmail.com>
2022-06-21 07:04:50 +0530

    Update roboshop-shell-scirpt/

--- components/mysql.sh
+++ components/mysql.sh
@@ -5,7 +5,7 @@ CHECK_ROOT
 if [ -z "${MYSQL_PASSWORD}" ]; then
   echo "Need MYSQL_PASSWORD env variable"
   exit 1
-fi 
+fi
 
 PRINT "Configure YUM Repos"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
@@ -19,7 +19,7 @@ CHECK_STAT $?
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 PRINT "RESET Root Password"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}" &>>${LOG}
 CHECK_STAT $?
 
 exit
