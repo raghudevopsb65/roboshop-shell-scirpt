5284bd60b8e6b2358f9f1386e02bf3a71be5f94d
Raghu K <raghuk.vit@gmail.com>
2022-06-21 06:17:47 +0530

    Update roboshop-shell-scirpt/

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,30 +1,4 @@
 source components/common.sh
 
-CHECK_ROOT
-
-PRINT "Setting Up NodeJS YUM Repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-CHECK_STAT $?
-
-PRINT "Installing NodeJS"
-yum install nodejs -y &>>${LOG}
-CHECK_STAT $?
-
-
-useradd roboshop
-
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-cd /home/roboshop
-rm -rf catalogue
-unzip /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-npm install
-
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
-
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl restart catalogue
-systemctl enable catalogue
-
+COMPONENT=catalogue
+NODEJS
