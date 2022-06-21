db3beae78d102cb0db9a205433ff4858ceeb2eda
Raghu K <raghuk.vit@gmail.com>
2022-06-21 07:13:33 +0530

    Update roboshop-shell-scirpt/

--- components/mysql.sh
+++ components/mysql.sh
@@ -33,9 +33,11 @@ if [ $? -eq 0 ]; then
 fi
 
 
+PRINT "Download Schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
 
-#curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-#cd /tmp
-#unzip -o mysql.zip
-#cd mysql-main
-#mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql
+PRINT "Load Schema"
+cd /tmp && unzip -o mysql.zip &>>${LOG} && cd mysql-main && mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql &>>${LOG}
+CHECK_STAT $?
+
