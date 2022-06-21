c7c1fda65551c0c8905e1360ef4c87b1f279832c
Raghu K <raghuk.vit@gmail.com>
2022-06-21 06:19:30 +0530

    Update roboshop-shell-scirpt/

--- components/user.sh
+++ components/user.sh
@@ -1,21 +1,4 @@
 source components/common.sh
 
-CHECK_ROOT
-
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
-cd /home/roboshop
-rm -rf user
-unzip /tmp/user.zip
-mv user-main user
-cd /home/roboshop/user
-npm install
-
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl restart user
-systemctl enable user
+COMPONENT=user
+NODEJS
